566693e4b3c89bc4eb43331c4b0b104780ba8bb4
mikami28 <59552933+mikami28@users.noreply.github.com>
2020-02-02 16:46:08 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-
   root "message#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
